1bfb04712cc226b92c49185bb06dff5cd3df4699
Nila <43315617+nilathedragon@users.noreply.github.com>
2022-05-06 03:08:12 +0200

    Update util.go

--- pkg/cache/util.go
+++ pkg/cache/util.go
@@ -9,7 +9,7 @@ import (
 // ErrorCacheMiss is returned when the cache could not find the given key
 var ErrorCacheMiss = errors.New("the given key was not found in the cache")
 
-// CacheKeyHash returns the SHA256 hash of a string for caching purposes
+// KeyHash returns the SHA256 hash of a string for caching purposes
 func KeyHash(key string) string {
 	h := sha256.New()
 	h.Write([]byte(key))
