1dac517923480b1cdf3e6a642b1eda70334ebc05
github-action-benchmark <github@users.noreply.github.com>
2022-05-06 00:30:04 +0000

    add Benchmark (go) benchmark result for b870e7a80a34cb1f0b7d40d65969b394c6a02f9d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1651796642561,
+  "lastUpdate": 1651797004908,
   "repoUrl": "https://github.com/go-mojito/mojito",
   "entries": {
     "Benchmark": [
@@ -48,6 +48,54 @@ window.BENCHMARK_DATA = {
             "value": 2156034,
             "unit": "ns/op\t   27021 B/op\t     176 allocs/op",
             "extra": "543 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "michael.teuscher@pm.me",
+            "name": "Michael Teuscher",
+            "username": "nilathedragon"
+          },
+          "committer": {
+            "email": "michael.teuscher@pm.me",
+            "name": "Michael Teuscher",
+            "username": "nilathedragon"
+          },
+          "distinct": true,
+          "id": "b870e7a80a34cb1f0b7d40d65969b394c6a02f9d",
+          "message": "De-duplicate test code",
+          "timestamp": "2022-05-06T02:29:10+02:00",
+          "tree_id": "d60449427fe594955587462ab89d10f3e1f44dfc",
+          "url": "https://github.com/go-mojito/mojito/commit/b870e7a80a34cb1f0b7d40d65969b394c6a02f9d"
+        },
+        "date": 1651797004001,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "Benchmark_Router_Handler",
+            "value": 396443,
+            "unit": "ns/op\t   24093 B/op\t     176 allocs/op",
+            "extra": "2726 times\n2 procs"
+          },
+          {
+            "name": "Benchmark_Router_Handler_Not_Found",
+            "value": 349059,
+            "unit": "ns/op\t   29385 B/op\t     179 allocs/op",
+            "extra": "2984 times\n2 procs"
+          },
+          {
+            "name": "Benchmark_Router_Handler_With_Middleware_1",
+            "value": 339487,
+            "unit": "ns/op\t   29258 B/op\t     179 allocs/op",
+            "extra": "3255 times\n2 procs"
+          },
+          {
+            "name": "Benchmark_Router_Handler_With_Middleware_5",
+            "value": 2301348,
+            "unit": "ns/op\t   22883 B/op\t     175 allocs/op",
+            "extra": "3145 times\n2 procs"
           }
         ]
       }
