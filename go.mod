f533faaa75666b3540f76fb9f36b0d74c338830b
Michael Teuscher <michael.teuscher@pm.me>
2022-05-14 17:21:48 +0200

    Fix singular struct depndencies

--- go.mod
+++ go.mod
@@ -4,8 +4,10 @@ go 1.18
 
 require (
 	github.com/infinytum/injector v0.0.2
-	github.com/infinytum/structures v0.0.2
 	github.com/mitchellh/hashstructure/v2 v2.0.2
 )
 
-require github.com/julienschmidt/httprouter v1.3.0
+require (
+	github.com/infinytum/structures v0.0.1
+	github.com/julienschmidt/httprouter v1.3.0
+)
--- pkg/router/introspector_factory_default.go
+++ pkg/router/introspector_factory_default.go
@@ -33,13 +33,13 @@ func newDefaultHandlerArgFactory(forType reflect.Type) (HandlerIntrospectorArgFa
 
 	ctx := reflect.New(forType).Interface()
 
-	// Structs are usually Context objects that hold one or multiple fields
-	// that must be filled with dependencies
 	if forType.Kind() == reflect.Struct {
-		if err := injector.Fill(ctx); err != nil {
-			// Perhaps its not a structure to fill but a singular struct dependency
-			if err2 := injector.InjectInto(ctx); err2 != nil {
-				return nil, fmt.Errorf("Dependency Injection failed: '%s' and '%s'", err, err2)
+		// First test if its a struct dependency
+		if err := injector.InjectInto(ctx); err != nil {
+			// If not, structs are usually Context objects that hold one or multiple fields
+			// that must be filled with dependencies
+			if err2 := injector.Fill(ctx); err2 != nil {
+				return nil, fmt.Errorf("dependency Injection failed: '%s' and '%s'", err, err2)
 			}
 		}
 	}
