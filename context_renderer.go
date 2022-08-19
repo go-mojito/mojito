00f910f01e7ed8043fc3d351156ab125e39285c9
Nila <43315617+nilathedragon@users.noreply.github.com>
2022-08-19 19:01:38 +0200

    Allow customization of the view cache ttl (#19)

--- context_renderer.go
+++ context_renderer.go
@@ -11,6 +11,7 @@ import (
 
 const viewCachePrefix = "view_cache_"
 const viewBagMetadataKey = "mojito.pkg.renderer.ViewBag"
+const viewCacheTTLKey = "mojito.pkg.renderer.ViewCacheTTL"
 
 type builtinRenderContext struct {
 	router.Context
@@ -42,11 +43,20 @@ func (ctx *builtinRenderContext) View(view string) error {
 	if err != nil {
 		return err
 	}
-	if err := cache.Set(viewCacheKey, render); err != nil {
-		return err
+
+	viewCacheTTL, ok := ctx.Metadata().GetOrDefault(viewCacheTTLKey, time.Second*5).(time.Duration)
+	if !ok {
+		panic("viewCacheTTL is not time.Duration")
 	}
-	if err := cache.ExpireAfter(viewCacheKey, time.Second*5); err != nil {
-		return err
+	if viewCacheTTL >= 0 {
+		if err := cache.Set(viewCacheKey, render); err != nil {
+			return err
+		}
+		if viewCacheTTL != 0 {
+			if err := cache.ExpireAfter(viewCacheKey, viewCacheTTL); err != nil {
+				return err
+			}
+		}
 	}
 
 	return ctx.String(render)
@@ -59,6 +69,15 @@ func (ctx *builtinRenderContext) ViewBag() renderer.ViewBag {
 		panic("ViewBag is not a renderer.ViewBag")
 	}
 	return viewBag
+}
+
+// SetViewCacheTTL sets the duration a rendered view is kept in the cache.
+// If the duration is 0, the view is cached forever.
+// If the duration is negative, the view is not cached.
+func (ctx *builtinRenderContext) SetViewCacheTTL(t time.Duration) {
+	if err := ctx.Context.Metadata().Set(viewCacheTTLKey, t); err != nil {
+		DefaultLogger().Errorf("failed to set view cache ttl: %s", err)
+	}
 }
 
 func NewRenderContext(ctx router.Context) RendererContext {
