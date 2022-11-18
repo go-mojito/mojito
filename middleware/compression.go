4fb4a1d301232c6ddab4dbc711542a0ae8c4534a
Cedric Lewe <0skillallluck@pm.me>
2022-11-18 02:07:59 +0100

    Add back missing check for Accept

--- middleware/compression.go
+++ middleware/compression.go
@@ -8,7 +8,8 @@ import (
 
 // Compression compresses the http response if supported by the client.
 func Compression(ctx mojito.Context, next func() error) (err error) {
-	if strings.Contains(ctx.Request().GetRequest().Header.Get("Connection"), "Upgrade") {
+	if strings.Contains(ctx.Request().GetRequest().Header.Get("Connection"), "Upgrade") ||
+		strings.Contains(ctx.Request().GetRequest().Header.Get("Accept"), "text/event-stream") {
 		return next()
 	}
 
