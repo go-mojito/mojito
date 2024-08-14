61f92f70f4cdfabfe0d48a64b6bf1dbc14d7150e
Cedric Lewe <0SkillAllLuck@pm.me>
2024-08-14 15:59:14 +0200

    Improve logger.Fields.Clone performance

--- pkg/logger/util.go
+++ pkg/logger/util.go
@@ -1,13 +1,11 @@
 package logger
 
+import "maps"
+
 // Fields is a type definition for cleaner code
 type Fields map[string]interface{}
 
 // Clone will clone a map of logfields into a new one
 func (f Fields) Clone() Fields {
-	m := make(Fields)
-	for n, v := range f {
-		m[n] = v
-	}
-	return m
+	return maps.Clone(f)
 }
