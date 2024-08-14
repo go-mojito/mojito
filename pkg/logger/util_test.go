381b94b4ab84761aa47020e32e8bf4cc42bbbab7
Cedric Lewe <cedric@qubyte.io>
2024-08-14 16:12:02 +0200

    perf: Improve Fields.Clone and withMojitoHandlerRouter (#94)
    * Improve logger.Fields.Clone performance
    
    * Improve performance of withMojitoHandlerRouter

--- pkg/logger/util_test.go
+++ pkg/logger/util_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestLogFields_Clone(t *testing.T) {
+func Test_Fields_Clone(t *testing.T) {
 	tests := []struct {
 		name string
 		f    Fields
@@ -38,4 +38,22 @@ func TestLogFields_Clone(t *testing.T) {
 			}
 		})
 	}
+}
+
+func Benchmark_Fields_Clone(b *testing.B) {
+	orig := Fields{
+		"a": "1",
+		"b": "2",
+		"c": "3",
+		"d": "4",
+		"e": "5",
+		"f": "6",
+	}
+
+	copies := make([]Fields, b.N)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		copies[i] = orig.Clone()
+	}
+	b.StopTimer()
 }
